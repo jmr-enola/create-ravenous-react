85abf7e8a7d94eddc95d9db075226a127086130d
jmr-enola <jmr.enola@gmail.com>
2024-04-10 19:02:20 +0800

    ravenous part-two complete

---
+++ src/mocks/dataDummy.js
@@ -0,0 +1,69 @@
+export const data = [
+   {
+        imgSrc: 'https://play-lh.googleusercontent.com/7AWR24mar0CgElbgJnKavdh-9bv2OQCJb15gpQkMVmBzoIHYdJqt1Lgr5I72liT3EA',
+        name: 'Pizza Hut',
+        address: 'East Service Road',
+        city: 'Taguig',
+        state: 'Philippines',
+        zipcode: '1630',
+        category: 'American',
+        rating: 4.5,
+        reviewCount: 201
+    }, 
+    {
+        imgSrc: 'https://play-lh.googleusercontent.com/7AWR24mar0CgElbgJnKavdh-9bv2OQCJb15gpQkMVmBzoIHYdJqt1Lgr5I72liT3EA',
+        name: 'Pizza Hut',
+        address: 'East Service Road',
+        city: 'Taguig',
+        state: 'Philippines',
+        zipcode: '1630',
+
+        category: 'American',
+        rating: 4.5,
+        reviewCount: 202
+    },
+    {
+        imgSrc: 'https://play-lh.googleusercontent.com/7AWR24mar0CgElbgJnKavdh-9bv2OQCJb15gpQkMVmBzoIHYdJqt1Lgr5I72liT3EA',
+        name: 'Pizza Hut',
+        address: 'East Service Road',
+        city: 'Taguig',
+        state: 'Philippines',
+        zipcode: '1630',
+        category: 'American',
+        rating: 4.5,
+        reviewCount: 203
+    },
+    {
+        imgSrc: 'https://play-lh.googleusercontent.com/7AWR24mar0CgElbgJnKavdh-9bv2OQCJb15gpQkMVmBzoIHYdJqt1Lgr5I72liT3EA',
+        name: 'Pizza Hut',
+        address: 'East Service Road',
+        city: 'Taguig',
+        state: 'Philippines',
+        zipcode: '1630',
+        category: 'American',
+        rating: 4.5,
+        reviewCount: 204
+    },
+    {
+        imgSrc: 'https://play-lh.googleusercontent.com/7AWR24mar0CgElbgJnKavdh-9bv2OQCJb15gpQkMVmBzoIHYdJqt1Lgr5I72liT3EA',
+        name: 'Pizza Hut',
+        address: 'East Service Road',
+        city: 'Taguig',
+        state: 'Philippines',
+        zipcode: '1630',
+        category: 'American',
+        rating: 4.5,
+        reviewCount: 205
+    },
+    {
+        imgSrc: 'https://play-lh.googleusercontent.com/7AWR24mar0CgElbgJnKavdh-9bv2OQCJb15gpQkMVmBzoIHYdJqt1Lgr5I72liT3EA',
+        name: 'Pizza Hut',
+        address: 'East Service Road',
+        city: 'Taguig',
+        state: 'Philippines',
+        zipcode: '1630',
+        category: 'American',
+        rating: 4.5,
+        reviewCount: 206
+    },
+];
